6c50bbd2ec31703a76c58717c37f132fcf0189b0
pengyaoc <pychen007@gmail.com>
2013-11-24 14:50:44 -0800

    Word Break 2 http://oj.leetcode.com/problems/word-break-ii/ Still can't pass large case in OJ

---
+++ WordBreak2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+void wordBreak(string s, int index, string current, int spaceAdded,
+                vector<string>& ret, unordered_set<string> &dict, vector<vector<bool> > &dp)
+{
+    string origin = current;
+    
+    for (int j=index; j<s.size(); j++)
+    {
+        if (dp[index][j])
+        {
+            if (j+1+spaceAdded != current.size())
+            {
+                current.insert(j+1+spaceAdded, " ");
+            }
+            else
+            {
+                ret.push_back(current);
+            }
+            wordBreak(s, j+1, current, spaceAdded+1, ret, dict, dp);
+        }
+        current = origin;
+    }
+}
+
+vector<string> wordBreak(string s, unordered_set<string> &dict) {
+    vector<string> ret;
+    
+    if (s.size() == 0)
+        return ret;
+        
+    vector<vector<bool> > dp(s.size(), vector<bool>(s.size(), false));
+    
+    for (int i=0; i<s.size(); i++)
+    {
+        for (int j=i; j<s.size(); j++)
+        {
+            dp[i][j] = (dict.find(s.substr(i, j-i+1)) != dict.end());
+        }
+    }
+        
+    wordBreak(s, 0, s, 0, ret, dict, dp);
+    return ret;
+}
+
+int main()
+{
+    unordered_set<string> dict = {"cat", "cats", "and", "sand", "dog"};
+    string s = "catsanddog";
+    vector<string> out = wordBreak(s, dict);
+    for (int i=0; i<out.size(); i++)
+    {
+        cout << out[i] << endl;
+    }
+}
