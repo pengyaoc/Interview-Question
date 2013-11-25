431abf6f2314f37b27b2b92ee1939e110066f93b
pengyaoc <pychen007@gmail.com>
2013-11-24 16:31:24 -0800

    Passed

--- WordBreak2.cpp
+++ WordBreak2.cpp
@@ -3,11 +3,10 @@
 #include <unordered_set>
 using namespace std;
 
-void wordBreak(string s, int index, string current, int spaceAdded,
+/*void wordBreak(string s, int index, string current, int spaceAdded,
                 vector<string>& ret, unordered_set<string> &dict, vector<vector<bool> > &dp)
 {
-    string origin = current;
-    
+    string origin = current;   
     for (int j=index; j<s.size(); j++)
     {
         if (dp[index][j])
@@ -43,6 +42,48 @@ vector<string> wordBreak(string s, unordered_set<string> &dict) {
     }
         
     wordBreak(s, 0, s, 0, ret, dict, dp);
+    return ret;
+}*/
+
+void backTrace(vector<vector<int> >& bt, int index, string s, string current, vector<string>& ret)
+{
+    string origin = current;
+    
+    if (index == -1)
+    {
+        ret.push_back(current);
+        return;
+    }
+    
+    for (int j=0; j<bt[index].size(); j++)
+    {
+        string x;
+        if (current.size() != 0)
+            x = " " + current;
+        current = s.substr(bt[index][j], index-bt[index][j]+1) + x;
+        backTrace(bt, bt[index][j]-1, s, current, ret);
+        current = origin;
+    }
+}
+
+vector<string> wordBreak(string s, unordered_set<string> &dict) {    
+    vector<string> ret;
+    if (s.size() == 0)
+        return ret;
+        
+    vector<vector<int> > bt(s.size());
+    
+    for (int i=0; i<s.size(); i++)
+    {
+        for (int j=i; j<s.size(); j++)
+        {
+            if (dict.find(s.substr(i, j-i+1)) != dict.end())
+            {
+                bt[j].push_back(i);
+            }
+        }
+    }
+    backTrace(bt, s.size()-1, s, "", ret);
     return ret;
 }
 
