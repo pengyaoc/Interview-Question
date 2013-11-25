5310e29464e60f6dc01591abe6529e90e0554f11
pengyaoc <pychen007@gmail.com>
2013-11-24 17:05:39 -0800

    With Memorization

--- WordBreak.cpp
+++ WordBreak.cpp
@@ -3,20 +3,39 @@
 #include <unordered_set>
 using namespace std;
 
-bool wordBreak(string s, int index, unordered_set<string> &dict)
+bool wordBreak(string s, int index, unordered_set<string> &dict, vector<int>& mem)
 {
+    bool ret = false;
     for (int i=index; i<s.size(); i++)
     {
-        if (dict.find(s.substr(index, i-index+1)) != dict.end() && wordBreak(s, i+1, dict))
+        if (dict.find(s.substr(index, i-index+1)) != dict.end())
         {
-            return true;
+            if (i+1 == s.size())
+            {
+                ret = true;
+                break;
+            }
+            if (mem[i+1] == -1)
+            {
+                wordBreak(s, i+1, dict, mem);
+            }
+            if (mem[i+1] == 1)
+            {
+                ret = true;
+                break;
+            }
         }
     }
-    return false;
+    mem[index] = ret ? 1 : 0;
+    return ret;
 }
 
 bool wordBreak(string s, unordered_set<string> &dict) {
-    return wordBreak(s, 0, dict);
+    vector<int> mem(s.size(), -1);
+    return wordBreak(s, 0, dict, mem);
 }
 
-int main() {}
+int main() {
+    unordered_set<string> a = {"aaaa", "aaa", "aaaaaaaaaa"};
+    cout << wordBreak("aaaaaaaaaaaaaa", a) << endl;
+}
