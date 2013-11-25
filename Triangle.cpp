db7c86b0d8ae8717169e22d6201d199375267b81
pengyaoc <pychen007@gmail.com>
2013-11-24 23:39:17 -0800

    Simplify

--- Triangle.cpp
+++ Triangle.cpp
@@ -3,23 +3,12 @@ int minimumTotal(vector<vector<int> > &triangle) {
     {
         return 0;
     }
-    else if (triangle.size() == 1)
-    {
-        return triangle[0][0];
-    }
-    vector<int> sum(triangle.size()-1, 0);
+    vector<int> sum(triangle.size(), 0);
     for (int i=triangle.size()-1; i>=0; i--)
     {
         for (int j=1; j<triangle[i].size(); j++)
         {   
-            if (i == triangle.size()-1)
-            {
-                sum[j-1] = min(triangle[i][j-1], triangle[i][j]);
-            }
-            else
-            {
-                sum[j-1] = min(sum[j-1]+triangle[i][j-1], sum[j]+triangle[i][j]);
-            }
+            sum[j-1] = min(sum[j-1]+triangle[i][j-1], sum[j]+triangle[i][j]);
         }
         
     }
